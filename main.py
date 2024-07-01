ee9772bfe370a72ef98a8b31f810cb53ceb7fca3
Andreu Llopis <130099847+PrMestizo@users.noreply.github.com>
2024-07-01 18:28:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
+from langchain_pinecone import PineconeVectorStore
+
+
+if __name__ == "__main__":
+
+    pdf_directory = "Data"
+    documents = []
+
+    for filename in os.listdir(pdf_directory):
+        if filename.endswith(".pdf"):
+            file_path = os.path.join(pdf_directory, filename)
+            loader = PyPDFLoader(file_path=file_path)
+            documents.extend(loader.load())
+
+    text_splitters = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitters.split_documents(documents=documents)
+
+    embeddings = OpenAIEmbeddings()
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retrieval_qa_chat_prompt
+    )
+    retrieval_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke({"input": "Que ocurrio el 6 de diciembre de 2020? "})
+    print(res["answer"])
+
+
